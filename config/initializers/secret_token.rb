714f3cd65a85c72834156ec977946a697b81a79f
Wolfram Arnold <wolfram@rubyfocus.biz>
2010-11-08 17:48:31 -0800

    First commit, skeleton with gemspec for rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'c445b343a51bf3e34a7303dc93a36e10f67d93c1df5476ad493b6bf022443c853550586977287b9e2da77370eda613e12d14a1ab2bc4d8b3f20fd682a752f9b5'
