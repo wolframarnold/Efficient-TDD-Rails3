92b3cf5b7d3f8323c960c10b2042758ee1496733
Wolfram Arnold <wolfram@rubyfocus.biz>
2010-11-08 18:09:34 -0800

    Validate presence first, last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  # Rails 2.3
+  # validates_presence_of :first_name
+
+  # Rails 3
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,15 @@ describe User do
       User.create(:first_name => "Joe", :last_name => "Smith")
     }.should change(User, :count).by(1)
   end
+
+  context "is not valid" do
+    subject { User.new }
+
+    [:first_name, :last_name].each do |attr|
+      it "without #{attr}" do
+        subject.should_not be_valid
+        subject.errors.on(attr).should_not be_nil
+      end
+    end
+  end
 end
